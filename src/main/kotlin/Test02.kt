39d055b218bcfd50c3f5b85de56435b830a692fd
Oleg Chumin <tschumin.oleg@gmail.com>
2023-02-26 21:34:04 +0300

    Add new files

---
+++ src/main/kotlin/Test02.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("Testing nice Kotlin")
+    var a = 5
+    var b = 9
+    val c = a++ * 1 - b--
+    println(c)
+
+    println("UByte.MAX_VALUE ${UByte.MAX_VALUE}")
+    println("UShort.MAX_VALUE ${UShort.MAX_VALUE}")
+    println("UInt.MAX_VALUE ${UInt.MAX_VALUE}")
+}
