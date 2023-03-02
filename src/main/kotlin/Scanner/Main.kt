35f390b181a60ebfee086eb636b64c6df6360bfc
Oleg Chumin <tschumin.oleg@gmail.com>
2023-03-02 14:15:43 +0300

    Add new files

---
+++ src/main/kotlin/Scanner/Main.kt
@@ -0,0 +1,33 @@
+package Scanner
+
+import java.util.Scanner
+
+//        debit = scanner.useDelimiter(System.getProperty("line.separator")).nextInt()
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    var balance = scanner.nextLine().toInt()
+    var debit: Int
+    var isPositiveBalance = false
+
+    if (scanner.hasNextLine()) {
+        val lineOfPurchasesSequence = scanner.nextLine()
+        val lineScan = Scanner(lineOfPurchasesSequence)
+
+        while (lineScan.hasNextInt()) {
+            debit = lineScan.nextInt()
+            if (debit > balance) {
+                println("Error, insufficient funds for the purchase. Your balance is $balance, but you need $debit.")
+                isPositiveBalance = false
+                lineScan.close()
+                break
+            }
+            balance -= debit
+            isPositiveBalance = true
+        }
+        lineScan.close()
+    }
+    if (isPositiveBalance) {
+        println("Thank you for choosing us to manage your account! Your balance is $balance.")
+    }
+}
