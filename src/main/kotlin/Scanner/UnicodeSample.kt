0360083aba458a0adba83716c91364655fe9aa84
Oleg Chumin <tschumin.oleg@gmail.com>
2023-03-05 12:32:25 +0300

    Add new files

---
+++ src/main/kotlin/Scanner/UnicodeSample.kt
@@ -0,0 +1,10 @@
+package Scanner
+
+fun main() {
+    print("" + '\u0048' + '\u0079' + '\u0070' + '\u0065' + '\u0072')
+    print('\u0073')
+    print('\u006B')
+    print('\u0069')
+    print('\u006C')
+    print('\u006C')
+}
