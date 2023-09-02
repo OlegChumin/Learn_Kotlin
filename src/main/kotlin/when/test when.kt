3f756b380134db2e61cd3fefbe3d8bec40382710
Oleg Chumin <tschumin.oleg@gmail.com>
2023-09-02 08:40:09 +0300

    CRUD code

---
+++ src/main/kotlin/when/test when.kt
@@ -0,0 +1,17 @@
+package `when`
+
+fun main() {
+
+    var a = 10
+
+    when (a) {
+//        > 0 -> print("Ok")
+//        in 1..20 -> print("Ok"); print("Ok")
+//            a +a -> print("Ok")
+//        in 1..22 -> print("Ok")
+//
+//        11, 12 -> print("Ok")
+//
+//        "2" -> print("Ok")
+    }
+}
