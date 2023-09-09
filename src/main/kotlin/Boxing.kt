c3fbc06e5d4c0c074921e3d8441046e7853e936d
OlegChumin <tschumin.oleg@gmail.com>
2023-09-09 06:30:12 +0300

    CRUDE code

---
+++ src/main/kotlin/Boxing.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val i = 101
+    val d = 3.1415
+    val s = "32"
+
+//    i.toBoolean()
+
+    i.toDouble()
+
+    d.toLong()
+
+    d.toString()
+
+//    s.toChar()
+
+    s.toInt()
+
+}
