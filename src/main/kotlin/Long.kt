cf530eb5a624e2cd2c7732d44565c781009fa738
OlegChumin <tschumin.oleg@gmail.com>
2023-10-08 19:37:23 +0300

    Add Long for magic + giant task

---
+++ src/main/kotlin/Long.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val magic = 2_000_000_000
+    val giant = 3_000_000_000
+// insert here
+//    val x = (magic + giant) % 10
+
+//    val x = 100L - magic
+//
+//    val x = giant - magic
+//
+    val x = magic + magic + 1
+}
